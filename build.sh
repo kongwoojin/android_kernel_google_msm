d2f20da0e600566f90bfbfecba8e0c804a4bd7cf
kongwoojin <kongwoojin03@gmail.com>
2020-09-19 21:27:49 +0900

    defconfig: add kongjak_defconfig

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 export ARCH=arm
 export CROSS_COMPILE=$(pwd)/../arm-eabi-4.8/bin/arm-eabi-
 
-make followmsi_defconfig
+make kongjak_defconfig
 make -j$(nproc)
 
 cp $(pwd)/arch/arm/boot/zImage $(pwd)/zImage
